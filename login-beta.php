d026cc2b05d44869509c454edb73b69c303d9d84
Eric Adams <ericadams@Thor.(none)>
2012-10-27 12:37:33 -0600

    extracted login and sign to separate files

--- login-beta.php
+++ login-beta.php
@@ -1,8 +1,10 @@
 <?php
 ob_start();
+//print_r($_POST);
 error_reporting(E_ALL ^ E_NOTICE);
 include_once ('connect.php');
-include_once ('salt.php');
+include_once ('config/salt.php');
+//print_r($_POST);
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 900)) {
     // last request was more than 30 minutes ago clear session info
     session_unset();     // unset $_SESSION variable for the runtime 
@@ -10,39 +12,51 @@ if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 
 }
 if (!isset($_SESSION['LAST_ACTIVITY'])) {
 session_start();
+//var_dump($_POST);
+$_SESSION['LAST_ACTIVITY'] = time(); 
+$time = time();
+$_SESSION['loginTime'] = $time;
 	if (isset($_POST['email']) and isset($_POST['password'])) {
 		$user = $_POST['email'];
 		$pwd = sha1($_POST['password'].$pepper);
-		$sql = "SELECT COUNT(id) FROM users WHERE email='$user' AND password='$pwd'";
 		$sql1 = "SELECT salt FROM users WHERE email='$user' AND password='$pwd'";
-		$sql2 = "SELECT email,password FROM users WHERE email='$user'";
+		$sql2 = "SELECT email, password FROM users WHERE email='$user'";
 		$result= mysql_query($sql2,$link);
-		$email = mysql_fetch_array($result); 
-		if($email[0] == $user){ 
+		$email = mysql_fetch_array($result);
+		//echo $email[0];
+		//exit(); 
+		if($email[0] == $user){
+			//echo "we have a user match!";
+			$sql = "SELECT COUNT(id) FROM users WHERE email='$user' AND password='$pwd'"; 
 			$result = mysql_query($sql, $link);
-			$result2 = mysql_query($sql1, $link);
-			list($salt1) = mysql_fetch_array($result2);
 			list($count) = mysql_fetch_array($result);
+			//echo $count;
 			if ($count > 0) {
+				$result = mysql_query($sql1, $link);
+				list($salt1) = mysql_fetch_array($result);
+				echo $salt1;
 				$_SESSION['salt'] = $salt1;
 				$_SESSION['pwd'] = $pwd;
 				$_SESSION['user'] = $user;
-				$_SESSION['LAST_ACTIVITY'] = time(); 
-				$time = time();
-				$_SESSION['loginTime'] = $time;
 				$hash = sha1($user.$time.$salt1);
 				$_SESSION['hash'] = $hash;
-				//header('location:fluid.php');
-				if (isset($_GET['file'])) {
-					$file = $_GET['file'];
-					header('location:'.$file);
+				//echo "all session variables set";
+				//exit();
+				if (isset($_POST['file'])) {
+					$file = $_POST['file'];
+					header('location:auth_user.php'.$file);
 					}else{
-						header('location:fluid.php');
+						header('location:auth_user.php');
 					}
-				}				header('location:fluid.php');
-			echo "Bad Username or Password. <a href='reset_password.php'>Reset Password?</a><br>";
+				}else{
+				$message = "1";
+				$_SESSION['message'] = $message;
+				header('location:login.php');
+				}
 			}else{
-		echo "Bad Username or Password. <a href='reset_password.php'>Reset Password?</a><br>";
+			$message = "1";
+			$_SESSION['message'] = $message;
+			header('location:login.php');
 		}
 	}
 }
