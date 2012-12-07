6892c388fbd2f25c59e3c8c96f30832efe78e80b
Eric Adams <ericjamesadams@gmail.com>
2012-12-07 12:37:59 -0500

    removed credential information

--- AWSSDKforPHP/config.inc.php
+++ AWSSDKforPHP/config.inc.php
@@ -54,11 +54,11 @@ CFCredentials::set(array(
 
 		// Amazon Web Services Key. Found in the AWS Security Credentials. You can also pass
 		// this value as the first parameter to a service constructor.
-		'key' => 'AKIAIAV4ZHI3QLWGL34Q',
+		'key' => '#################',
 
 		// Amazon Web Services Secret Key. Found in the AWS Security Credentials. You can also
 		// pass this value as the second parameter to a service constructor.
-		'secret' => 'ibzkQkHcT2EQ2l/s5cjKICUcyZvRbA0+ySz5X2sW',
+		'secret' => '################',
 
 		// This option allows you to configure a preferred storage type to use for caching by
 		// default. This can be changed later using the set_cache_config() method.
--- AWSSDKforPHP/rs-api-creds.php
+++ AWSSDKforPHP/rs-api-creds.php
@@ -2,8 +2,8 @@
           $rs_api = array(
 			'version' => '1.0',
 			'account_id' => 46971,
-			'username' => "eric.adams@pearson.com",
-			'password' => "g0r3ds0x!",
+			'username' => "############",
+			'password' => "#######",
 			'cookie_file' => 'tmp/rs_api_cookie.txt',
 			//'cookie_file' => tempnam("/tmp", "rs_api"),
 		);	
